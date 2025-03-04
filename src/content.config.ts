fb3a3c9f9313d53452e1c731196c602e0cb7fd84
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-03-04 15:39:14 +0700

    feat(init): init commit

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,21 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
 	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date(),
+		date: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+    author: z.string().optional(),
+    summary: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+export const collections = {
+  'blog': blogCollection,
+};
