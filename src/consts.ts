4383655444dbd8e53ccdc51b6d3fee0975708129
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2025-10-07 10:01:03 +0700

    feat(consts.ts): update consts value

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Muhammad Reza Ananta';
+export const SITE_DESCRIPTION = 'Welcome to my portfolio website, where you can view my past projects and contact me';
 export const ASCII_LOGO = `
 ███╗   ███╗      ██████╗ ████████╗████████╗ █████╗
 ████╗ ████║      ██╔══██╗███╔════╝╚═════██║██╔══██╗
@@ -17,9 +17,3 @@ export const ASCII_LOGO = `
 ██║  ██║██║ ╚███║██║  ██║██║ ╚███║   ██║   ██║  ██║
 ╚═╝  ╚═╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝  ╚══╝   ╚═╝   ╚═╝  ╚═╝
 `.trim();
-//  █████╗ ███╗  ██╗ █████╗ ███╗  ██╗████████╗ █████╗           Z
-// ██╔══██╗████╗ ██║██╔══██╗████╗ ██║╚══██╔══╝██╔══██╗       Z
-// ███████║██╔██╗██║███████║██╔██╗██║   ██║   ███████║    z
-// ██╔══██║██║╚████║██╔══██║██║╚████║   ██║   ██╔══██║ z
-// ██║  ██║██║ ╚███║██║  ██║██║ ╚███║   ██║   ██║  ██║
-// ╚═╝  ╚═╝╚═╝  ╚══╝╚═╝  ╚═╝╚═╝  ╚══╝   ╚═╝   ╚═╝  ╚═╝
